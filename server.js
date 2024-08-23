b71794682623a07265bc1a79f3db00b984e9c26e
NirmalNagaraj <nirmal070308@gmail.com>
2024-08-23 11:53:14 +0530

    feat: Added Aiven database

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const cors = require('cors');
 const fs = require('fs');
 const bodyParser = require('body-parser');
